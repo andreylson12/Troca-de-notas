013d85974456c8b332379d95ff875b8bd481073e
andreylson12 <Andreylsonvirtual@gmail.com>
2026-06-03 15:33:05 -0300

    Update robo-solum.js

--- robo-solum.js
+++ robo-solum.js
@@ -460,33 +460,42 @@ if(ehMotz){
   }
 
   // UF MOTZ: pega a UF que vem depois da PLACA CAVALO
-uf='';
+  uf='';
 
-const placaLinhaUF=textoLimpo.match(
-  /PLACA\s+CAVALO\s*[:.\s]*[A-Z]{3}[-\s]?\d[A-Z0-9][-\s]?\d{2}\s+CIDADE\s*:\s*(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)/i
-);
+  const placaLinhaUF=textoLimpo.match(
+    /PLACA\s+CAVALO\s*[:.\s]*[A-Z]{3}[-\s]?\d[A-Z0-9][-\s]?\d{2}\s+CIDADE\s*:\s*(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)/i
+  );
 
-if(placaLinhaUF){
-  uf=placaLinhaUF[1].toUpperCase();
-}
+  if(placaLinhaUF){
+    uf=placaLinhaUF[1].toUpperCase();
+  }
 
-if(!uf){
-  const blocoUF=textoLimpo.match(/PLACA\s+CAVALO[\s\S]{0,120}/i);
+  if(!uf){
+    const blocoUF=textoLimpo.match(/PLACA\s+CAVALO[\s\S]{0,120}/i);
 
-  if(blocoUF){
-    const ufsValidas=[...blocoUF[0].matchAll(/CIDADE\s*:\s*(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)/gi)]
-      .map(x=>x[1].toUpperCase())
-      .filter(u=>u.length===2);
+    if(blocoUF){
+      const ufsValidas=[...blocoUF[0].matchAll(/CIDADE\s*:\s*(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)/gi)]
+        .map(x=>x[1].toUpperCase())
+        .filter(u=>u.length===2);
 
-    if(ufsValidas.length){
-      uf=ufsValidas[0];
+      if(ufsValidas.length){
+        uf=ufsValidas[0];
+      }
     }
   }
-}
 
-    cnh=somenteNumero(
+  cnh=somenteNumero(
     achar(/CNH\s*[:.\s]*(\d{5,15})/i)
   );
+
+  // Correções OCR pontuais MOTZ
+  if(placaCavalo==='PJI1176'){
+    placaCavalo='RPJ1I76';
+  }
+
+  if(placaCavalo==='RPJI1176'){
+    placaCavalo='RPJ1I76';
+  }
 
   tipoBruto='RODOTREM 9 EIXO';
   tipoVeiculo='RODO-TREM 9 EIXO';
